60e2443df42114a4c677cdfcf76594996d1460f4
Raynaldi <raynss455x@gmail.com>
2024-09-19 16:31:15 +0700

    update again

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -19,18 +19,18 @@ const Navbar: React.FC = () => {
   };
 
   return (
-    <nav className="bg-white shadow-md w-full">
-      <div className='bg-gray-300 w-[800px] mx-auto'>
+    <nav className="w-full">
+      <div className='w-[800px] mx-auto'>
         <div className="mx-auto px-4 py-2 flex justify-between items-center relative">
 
           <div className='flex-row flex items-center'>
             <div className='w-[78px] h-[39px]'>
               <img src={`${process.env.PUBLIC_URL}/assets/tuaide-icon.png`} alt="Icon" className="h-full w-full" />
             </div>
-            <span className="text-lg roboto-medium p-2 mt-2 border border-vlack">INVOICE</span>
+            <span className="ml-2 text-lg roboto-medium px-1 py-1 mt-2 border border-black rounded-lg shadow-[5px_5px_0px_black] transition-transform hover:translate-x-1 hover:translate-y-1 ">INVOICE</span>
           </div>
           
-          <div className='flex flex-col relative ml-auto'> {/* Tambahkan ml-auto */}
+          <div className='flex flex-col relative ml-auto border p-1'> {/* Tambahkan ml-auto */}
             <div 
               className={`burger-icon ${isOpen ? 'open' : ''}`} 
               onClick={toggleDropdown}
